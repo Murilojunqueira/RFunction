377bd668d5ca603e0592c27f84e6f17f36b02961
Murilojunqueira <m.junqueira@yahoo.com.br>
2019-06-02 01:18:41 -0300

    Update github_PrivadeFunction.R

--- github_PrivadeFunction.R
+++ github_PrivadeFunction.R
@@ -8,15 +8,41 @@
 
 ################# Prepara Área de trabalho ########################
 
-#library(devtools)
-library(httr)
-
+library(devtools)
+library(tidyverse)
+library(RCurl)
 
 ################# Funções ########################
 
+Tutoriais
 
-github_PrivadeFunction.R <- function() {
+# https://www.r-bloggers.com/reading-an-r-file-from-github/
+# github_PrivadeFunction("NotNull")
+
+github_PrivadeFunction <- function(function.name) {
+  # function.name <- "NotNull"
+  Functions.BD.raw <- getURL("https://raw.githubusercontent.com/Murilojunqueira/RFunction/master/BD/Functions.csv")
   
+  Colunas <- c("Function.Id", "Function.Name", "Function.Desc",
+               "Function.DateCreation","Function.LastUpdate",
+               "Folder.Id", "Folder.Path")
   
+  Functions.BD <- Functions.BD.raw %>%
+    gsub('"""', "", .) %>% 
+    str_split("\n") %>% 
+    as.data.frame(stringsAsFactors = FALSE) %>% 
+    rename(Var1 = 1) %>% 
+    filter(Var1 != "") %>% 
+    slice(2:n()) %>% 
+    separate(1, Colunas, ";")
   
+  function.number <- which(Functions.BD$Function.Name == function.name)
+  
+  source.Path <- paste0("https://raw.githubusercontent.com/Murilojunqueira/RFunction/master/",
+                        Functions.BD$Folder.Path[function.number], 
+                        Functions.BD$Function.Name[function.number], ".R")
+  
+  source_url(source.Path)
 }
+
+# Fim
